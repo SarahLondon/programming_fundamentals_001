694152af4a4c3a5bb633db405e32198b3d61201e
SarahLondon <tucci.sarah@gmail.com>
2020-04-10 19:10:57 +0100

    Programming_fundamentals_001_for_review

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,33 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName (firstName, lastName){
+    let name = `${firstName} ${lastName}`
+    return name;
+}
+
+function makeHalfPrice(price) {
+    return price - (0.5 * price);
+}
+
+function countBooks(titles) {
+return titles.length;
+}
+
+function isInStock(book) {
+    return book.quantity >=1;
+    }
+
+function getTotalOrderPrice(price, quantity) {
+        return (price * quantity) + [(price * 0.2) * quantity];
+    }
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
+
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,68 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+});
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 150 when passed 300", () => {
+    expect(utils.makeHalfPrice(300)).toBe(150);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns 2 when passed 2", () => {
+    expect(utils.countBooks(["The Bible", "La Divina Commedia"])).toBe(2);
+  });
+});
+
+describe("utils.isInStock", () => {
+test("returns true if the title is in stock", () => {
+  const book = {
+    title: "The Bible",
+    author: "Unknown",
+    yearOfPublication: 1993,
+    quantity: 9
+  };
+
+  expect(utils.isInStock(book)).toBe(true);
+});
+
+test("returns true if the title is in stock", () => {
+  const book = {
+    title: "The Bible",
+    author: "Unknown",
+    yearOfPublication: 1993,
+    quantity: 0
+  };
+
+  expect(utils.isInStock(book)).toBe(false);
+});
+});
+
+describe("utils.getTotalOrderPrice", () => {
+  test("returns 60 when passed 2", () => {
+    expect(utils.getTotalOrderPrice(2)).toBe(60);
+  });
+});
+
+test("returns 43.2 when passed 18", () => {
+  expect(utils.getTotalOrderPrice(18)).toBe(43.2);
+});
+
+test("returns 43.2 when passed 2", () => {
+  expect(utils.getTotalOrderPrice(2)).toBe(43.2);
+});
+
+test("returns 43.2 when passed 36", () => {
+  expect(utils.getTotalOrderPrice(36)).toBe(43.2);
 });
